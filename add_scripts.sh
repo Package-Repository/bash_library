4e81df7c647d4562f68f35b9d148e0718d01d25d
Conner Sommerfield <conner.sommerfield@gmail.com>
2023-10-03 23:51:27 -0700

    Change Naming

--- add_scripts.sh
+++ add_scripts.sh
@@ -12,22 +12,36 @@ if [ ! -d "$TARGET_DIR" ]; then
     sudo chown -R "$(whoami)" "$TARGET_DIR"
 fi
 
-# Copy all subdirectories to the target directory
-for dir in "$SCRIPT_DIR"/*/; do
-    if [ -d "$dir" ]; then
-        cp -r "$dir" "$TARGET_DIR"
-    fi
-done
+add_all_directores_to_bash_rc()
+{
+    # Copy all subdirectories to the target directory
+    for dir in "$SCRIPT_DIR"/*/; do
+        if [ -d "$dir" ]; then
+            cp -r "$dir" "$TARGET_DIR"
+        fi
+    done
 
-# Update the PATH in ~/.bashrc
-echo "Adding directories to PATH in ~/.bashrc"
-for dir in "$TARGET_DIR"/*; do
-    if [ -d "$dir" ]; then
-        echo "export PATH=\$PATH:$dir" >> ~/.bashrc
-    fi
+    # Update the PATH in ~/.bashrc
+    echo "Adding directories to PATH in ~/.bashrc"
+    for dir in "$TARGET_DIR"/*; do
+        if [ -d "$dir" ]; then
+            echo "export PATH=\$PATH:$dir" >> ~/.bashrc
+        fi
+    done
+}
+
+# flag to add to bashrc
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -a|--add)
+           add_all_directores_to_bash_rc();;
+    esac
+    shift
 done
 
 # Refresh the shell configuration
 source ~/.bashrc
 
 echo "Setup complete. You can now use the scripts from the new directories."
+
+
