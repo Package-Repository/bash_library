df00a277bf65b0ac2b15c80b01a58ce294924305
Conner Sommerfield <conner.sommerfield@gmail.com>
2023-10-23 19:49:13 -0700

    Edit Checkout

--- github/checkout.sh
+++ github/checkout.sh
@@ -7,45 +7,43 @@ if [ $# -eq 0 ]; then
 fi
 
 # Directory to keep
-directory_to_keep="$1"
+# directory_to_keep="$1"
 
-username=$(whoami)
+# username=$(whoami)
 
-# Create a temporary directory for the copy
-temporary_copy_dir="/home/$username/temp/temporary_auto_gen_copy_dir"
-rm -rf "$temporary_copy_dir"
-mkdir -p "$temporary_copy_dir"
+# # Create a temporary directory for the copy
+# temporary_copy_dir="/home/$username/temp/temporary_auto_gen_copy_dir"
+# rm -rf "$temporary_copy_dir"
+# mkdir -p "$temporary_copy_dir"
 
 # Clone the Git repository
-git clone --recurse-submodules https://github.com/Repo-Factory/Robot-Packages git_auto_gen_package_dir
+git clone --recurse-submodules https://github.com/Repo-Factory-Mechatronics/$1
 
 # Move into the cloned repository directory
-cd git_auto_gen_package_dir || exit 1
+# cd git_auto_gen_package_dir || exit 1
 
-# Find all directories matching the specified name and copy them to the temporary directory
-find . -type d -name "$directory_to_keep" -exec sudo cp -r {} "$temporary_copy_dir/" \;
+# # Find all directories matching the specified name and copy them to the temporary directory
+# find . -type d -name "$directory_to_keep" -exec sudo cp -r {} "$temporary_copy_dir/" \;
 
-# Move back to the original directory
-cd ..
+# # Move back to the original directory
+# cd ..
 
-# Remove the cloned repository
-sudo rm -rf git_auto_gen_package_dir
+# # Remove the cloned repository
+# sudo rm -rf git_auto_gen_package_dir
 
-echo "Cleanup completed. Only '$directory_to_keep' and its subdirectories remain in $temporary_copy_dir."
+# echo "Cloned $1"
 
-# Move into the temporary directory
-cd "$temporary_copy_dir"
+# # Move into the temporary directory
+# cd "$temporary_copy_dir"
 
-sudo chown -R "$username" "$directory_to_keep"
+# sudo chown -R "$username" "$directory_to_keep"
 
-cd "$directory_to_keep"
-
-s
-. s
+# cd "$directory_to_keep"
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
         -b|--build)
+            . s
             bp
             bp -i            
             exit 0
